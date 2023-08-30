3cbb9ef236fc7a4f571b19584bf67fb2ae52ae23
AliHMohammad <alihmohammad@hotmail.com>
2023-08-30 14:10:33 +0200

    Refactor code

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,15 +1,12 @@
-import { getArtists, artistsList} from "./api.js";
-import { Artist } from "./interface.js";
+import { getArtists } from "./api.js";
 import { searchArtists } from "./search.js";
 import { initiateEventListeners } from "./helpers.js";
 
 window.addEventListener("load", main);
 
-
 async function main(event: Event): Promise<void> {
     console.log("App is running");
     await getArtists();
     searchArtists();
-    initiateEventListeners()
+    initiateEventListeners();
 }
-
